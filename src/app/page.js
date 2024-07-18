79eba857c40bb9ab2915183c5756721733d826a7
rizkyyy24 <95656791+rizkyyy24@users.noreply.github.com>
2024-07-18 18:27:10 +0700

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,19 +6,22 @@ export default function Home() {
     {
       title: "Facebook",
       url: "https:/facebook.com",
+      icon: "https://cdn.simpleicons.org/facebook/white",
     },
     {
-      title: "Twitter",
+      title: "Instagram",
       url: "https:/twitter.com",
+      icon: "https://cdn.simpleicons.org/instagram/white",
     },
     {
       title: "Tiktok",
       url: "https:/tiktok.com",
+      icon: "https://cdn.simpleicons.org/tiktok/white",
     },
   ];
 
   return (
-    <main className="flex min-h-screen mx-auto max-w-sm flex-col items-stretch justify-center py-12 px-4 bg-slate-100 shadow-md space-y-12">
+    <main className="flex min-h-screen mx-auto max-w-sm flex-col justify-center py-12 px-4 bg-slate-100 shadow-md space-y-12">
       <div className="w-[250px] h-[250px] rounded-full bg-blue-500 overflow-hidden self-center">
         <Image src={profil} width={500} height={500} />
       </div>
@@ -39,8 +42,9 @@ export default function Home() {
                 className="hover:translate-y-1 hover:shadow-xl transition duration-200"
               >
                 <a href={link.url} target="_blank">
-                  <div className="bg-blue-500 px-6 py-3 rounded-xl">
-                    {link.title}
+                  <div className="bg-blue-500 px-4 py-3 rounded-xl flex items-center">
+                    <img src={link.icon} width="25px" className="mr-4" />
+                    <h1>{link.title}</h1>
                   </div>
                 </a>
               </li>
@@ -49,7 +53,7 @@ export default function Home() {
         </ul>
       </div>
 
-      <div className="self-center">
+      <div className="self-center font-semibold">
         <h3>Copyright @Rizkyyoga</h3>
       </div>
     </main>
